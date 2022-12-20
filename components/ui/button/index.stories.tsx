2580faf02afb832ea2423c107d6fa7cf1846d167
marromugi <marrromugi@gmail.com>
2022-12-20 14:41:16 +0900

    init

---
+++ components/ui/button/index.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './';
+
+export default {
+  title: 'next-with-storybook/button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'select', options:["black", "white"] },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Secondary',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Large',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Small',
+};
